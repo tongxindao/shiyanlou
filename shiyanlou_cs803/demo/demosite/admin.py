50bfb91dda7554c45906d55e78048acb39a04111
tongxindao <balin1991@qq.com>
2017-09-04 13:43:52 +0800

    2.django admin

--- shiyanlou_cs803/demo/demosite/admin.py
+++ shiyanlou_cs803/demo/demosite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_data', 'update_time',)
+
+admin.site.register(Article,ArticleAdmin)
